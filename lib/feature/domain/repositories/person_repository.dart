4d990eb90b045310a2eef3dd19abdbd296f5b071
Home <dgryzhkov@gmail.com>
2021-09-09 11:09:44 +0700

    add Entity and Equatable

--- lib/feature/domain/repositories/person_repository.dart
+++ lib/feature/domain/repositories/person_repository.dart
@@ -1,6 +1,8 @@
+import 'package:rick_and_morty/core/error/failure.dart';
 import 'package:rick_and_morty/feature/domain/entities/person_entity.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class PersonRepository {
-  Future<List<PersonEntity>>getAllPerson(int page);
-  Future<List<PersonEntity>>searchPerson(String query);
+  Future<Either<Failure,List<PersonEntity>>>getAllPerson(int page);
+  Future<Either<Failure,List<PersonEntity>>>searchPerson(String query);
 }
